91983880222dbb9643367dcf4fc750b4806b9ded
Ava Shafran <avajtasg@gmail.com>
2024-12-06 22:14:31 -0800

    Delete Test test.py

--- Test test.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-
-import kagglehub
-
-# Download latest version of dataset
-path = kagglehub.dataset_download("noamsegal/affectnet-training-data")
-
-print("Path to dataset files:", path)
